8c048c0f5bcba075c7cc7388fe7a360b196205d1
fujii <robo_human@fuji.waseda.jp>
2022-12-12 01:29:34 +0900

    プロジェクト ファイルを追加します。

---
+++ MineSweeper_2022/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace MineSweeper_2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(100, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(555, 285);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(380, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(150, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "↑";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(30, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "←";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(270, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "→";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(150, 300);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "↓";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(30, 450);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 50);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "オープン";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(240, 450);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 50);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "フラグ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(1900, 200);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 550);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "操作パネル";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2374, 1529);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "MineSweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ MineSweeper_2022/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper_2022
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        const int W = 37;               // 縦のマス
+        const int H = 19;               // 横のマス
+        public Form1()
+        {
+            InitializeComponent();
+            //Console.WriteLine(pictureBox1.Width);
+            //Console.WriteLine(pictureBox1.Height);
+            pictureBox1.Width = W * 22;
+            pictureBox1.Height = H * 22;
+            bmp = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+
+            //pictureBoxに，bmpをはりつける
+            this.pictureBox1.Image = bmp;
+
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+        }
+
+        const int NCell = W * H;        // すべてのマスの数
+        const int NMine = NCell / 10;   // 地雷の数
+
+        public enum MineState   // マスの地雷状態
+        {
+            IsSafe0 = 0,        // 安全（周辺地雷0個）
+            IsSafe1 = 1,        // 安全（周辺地雷1個）
+            IsSafe2 = 2,        // 安全（周辺地雷2個）
+            IsSafe3 = 3,        // 安全（周辺地雷3個）
+            IsSafe4 = 4,        // 安全（周辺地雷4個）
+            IsSafe5 = 5,        // 安全（周辺地雷5個）
+            IsSafe6 = 6,        // 安全（周辺地雷6個）
+            IsSafe7 = 7,        // 安全（周辺地雷7個）
+            IsSafe8 = 8,        // 安全（周辺地雷8個）
+            IsMine              // 地雷
+        };
+        public enum OpenState   // マスのオープン状態 
+        {
+            UnopenNone,         // 未オープン（旗なし）
+            UnopenFlag,         // 未オープン（旗あり）
+            Opened              // 既オープン
+        };
+
+        MineState[,] BoardMine = new MineState[W, H];   // 盤面地雷状態
+        OpenState[,] BoardOpen = new OpenState[W, H];   // 盤面オープン状態
+        bool IsPeekMode = false;                        // 裏舞台モードか
+        bool IsFinished = false;                        // 終了しているか
+        int LeftMine = NMine;                           // 残り地雷数
+        int LeftOpen = NCell - NMine;                   // 残り安全マス数
+        int CursorX = 0;                                // カーソルx座標
+        int CursorY = 0;                                // カーソル座標
+
+        MineState CountMine(int X, int Y)                                       // 周辺地雷カウント
+        {
+            int count = 0;
+            if (BoardMine[X,Y] == MineState.IsMine) return MineState.IsMine;    // 地雷なら地雷を返す
+            for(int j = Y - 1; j <= Y + 1 ; j++)
+            {
+                if (j == -1) continue;                  // 端対策
+                if (j == H) continue;
+                for (int i = X-1; i <= X + 1; i++)
+                {
+                    if (i == -1) continue;              // 端対策
+                    if (i == W) continue;
+                    if (i == X && j == Y) continue;                             // そのマスそのものは飛ばす
+                    else if (BoardMine[i,j] == MineState.IsMine) count++;       // 地雷ならカウント+1
+                }
+            }
+            return (MineState)Enum.ToObject(typeof(MineState), count);          // 地雷状態を返す
+        }
+
+        void InitializeBoard()
+        {
+            var rnd = new Random();
+
+            //初期配置生成
+            {
+                var temp = new int[H * W];
+                for (int i = 0; i < temp.Length; i++)
+                    temp[i] = i;
+                for (int i = temp.Length; --i > 0;)
+                {
+                    var j = rnd.Next(i + 1);
+                    (temp[i], temp[j]) = (temp[j], temp[i]);
+                }
+
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    int xx = temp[i] % W;
+                    int yy = temp[i] / W;
+                    var s = MineState.IsSafe0;
+                    if (i < temp.Length * 0.1)
+                    {
+                        s = MineState.IsMine;
+                    }
+                    BoardMine[xx, yy] = s;
+                    Console.WriteLine(s.ToString());
+                    BoardMine[xx, yy] = CountMine(xx,yy);
+                    BoardOpen[xx, yy] = OpenState.UnopenNone;
+                }
+            }
+        }
+
+        void OpenCell(int X, int Y)                         // オープン関数
+        {
+            if (BoardOpen[X, Y] != OpenState.UnopenNone)    // 旗ナシ未オープンでないならreturn
+            {
+                return;
+            }
+
+            switch (BoardMine[X, Y])
+            {
+                case MineState.IsSafe0:                         // 周りに地雷が0なら周りを全部オープン
+                    for (int j = Y - 1; j <= Y + 1; j++)
+                    {
+                        if (j == -1) continue;                  // 端対策
+                        if (j == H) continue;
+                        for (int i = X - 1; i <= X + 1; i++)    
+                        {
+                            if (i == -1) continue;              // 端対策
+                            if (i == W) continue;
+                            if (BoardOpen[i, j] == OpenState.UnopenNone)    // 周りのマスが旗アリなどの場合はスキップ
+                            {
+                                BoardOpen[i, j] = OpenState.Opened;
+                                LeftOpen--;
+                            }
+
+                        }
+                    }
+                    break;
+                case MineState.IsMine:                          // 地雷をオープンしたら
+                    BoardOpen[X, Y] = OpenState.Opened;         // オープンして
+                    LeftMine--;                                 // 地雷数を1減らして
+                    IsFinished = true;                          // 終了フラグをたてる
+                    break;
+                default:
+                    BoardOpen[X, Y] = OpenState.Opened;         // それ以外なら1マスだけオープン
+                    LeftOpen--;
+                    break;
+
+            }
+            if (LeftOpen == 0) IsFinished = true;               // 全部オープンしたら終了フラグ
+        }
+
+        void FlagCell(int X, int Y)                         // 旗をたてる
+        {
+            switch (BoardOpen[X, Y])                        // マスのオープン状態が
+            {
+                case OpenState.Opened:                      // オープンならreturn
+                    break;
+                case OpenState.UnopenNone:                  // 旗ナシなら旗をたてる
+                    BoardOpen[X, Y] = OpenState.UnopenFlag;
+                    LeftMine--;
+                    break;
+                case OpenState.UnopenFlag:                  // 旗アリなら旗をはずす
+                    BoardOpen[X, Y] = OpenState.UnopenNone;
+                    LeftMine++;
+                    break;
+            }
+        }
+
+
+        void drawProcess()                                  // 描画関数
+        {
+            int dx = pictureBox1.Width / W;
+            int dy = pictureBox1.Height / H;
+
+            Graphics g = Graphics.FromImage(bmp);
+            Brush black = new SolidBrush(Color.FromArgb(127, 127, 127));
+            Brush green = new SolidBrush(Color.FromArgb(103, 224, 77));
+            Brush red = new SolidBrush(Color.FromArgb(222, 64, 164));
+            Brush white = new SolidBrush(Color.FromArgb(255, 255, 255));
+            Pen normalFrame = new Pen(white, 1);                                // 普通のマス用
+            Pen cursorFrame = new Pen(green, 4);                                // カーソル用
+            Font font = new Font("MS ゴシック", 12);                            // フォント設定
+
+            for (int j = 0; j < H; j++)
+            {
+                for (int i = 0; i < W; i++)
+                {
+                    RectangleF rect = new RectangleF(i * dx, j * dy, dx, dy);   // FillRectangleと文字描画のときの範囲指定
+
+                    if (BoardOpen[i, j] == OpenState.UnopenFlag) 
+                        g.FillRectangle(red, rect);// 旗アリの場合赤で描画
+                    else g.FillRectangle(black, rect);                          // それ以外を黒で描画
+
+                    int around = (int)BoardMine[i, j];                          // 周辺の地雷の個数
+                    if (BoardOpen[i, j] == OpenState.Opened)                    // オープンされたマス
+                    {
+                        switch (BoardMine[i, j])
+                        {
+                            case MineState.IsMine:                              // 地雷なら赤字でM
+                                g.DrawString("M", font, red, rect);
+                                break;
+                            default:                                            // 安全マスなら白字で地雷の個数
+                                g.DrawString(around.ToString(), font, white, rect);
+                                break;
+
+                        }
+                    }
+
+                    if (i == CursorX && j == CursorY){                          // カーソルがあるところは緑で縁取り
+                        g.DrawRectangle(cursorFrame, i * dx, j * dy, dx, dy);
+                    }
+                    else
+                    {
+                        g.DrawRectangle(normalFrame, i * dx, j * dy, dx, dy);   // カーソルのないところは黒で縁取り
+                    }
+                    label1.Text = "残り安全マス: " + LeftOpen.ToString();
+                    label2.Text = "残り地雷マス: " + LeftMine.ToString();
+                }
+            }
+
+            //pictureBoxの中身を塗り替える
+            pictureBox1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //↑ボタンの操作
+        {
+            if (IsFinished == true) return;                     // 終了済ならreturn
+            if (CursorY == 0) CursorY = H - 1;                  // 一番上にいるときは一番下に移動
+            else CursorY--;                                     // 一個上のマスに移動
+            drawProcess();
+            return;
+        }
+
+        private void button2_Click(object sender, EventArgs e)  // ←ボタンの操作
+        {
+            if (IsFinished == true) return;                     // 終了済ならreturn
+            if (CursorX == 0) CursorX = W - 1;                  // 一番左にいるときは一番右に移動
+            else CursorX--;                                     // 一個下のマスに移動
+            drawProcess();
+            return;
+        }
+
+        private void button3_Click(object sender, EventArgs e)  // →ボタンの操作
+        {
+            if (IsFinished == true) return;                     // 終了済ならreturn
+            if (CursorX == W - 1) CursorX = 0;                  // 一番右にいるときは一番左に移動
+            else CursorX++;                                     // 一個右のマスに移動
+            drawProcess();
+            return;
+        }
+
+        private void button4_Click(object sender, EventArgs e)  // ↓ボタンの操作
+        {
+            if (IsFinished == true) return;                     // 終了済ならreturn
+            if (CursorY == H - 1)  CursorY = 0;                  // 一番下にいるときは一番上に移動
+            else CursorY++;                                     // 一個下のマスに移動
+            drawProcess();
+            return;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (IsFinished == true) return;                     // 終了済ならreturn
+            OpenCell(CursorX,CursorY);                          // オープン
+            drawProcess();                                      // 描画し直し
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (IsFinished == true) return;                     // 終了済ならreturn
+            FlagCell(CursorX, CursorY);                         // フラグ
+            drawProcess();                                      // 描画し直し
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeBoard();                                  // 初期化
+            drawProcess();                                      // 描画し直し
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
