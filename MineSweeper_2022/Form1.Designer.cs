c48da0ed3fd874abb5514c39ba7343dd502f861c
Aoi FUJII <robo_human@fuji.waseda.jp>
2022-12-20 17:26:26 +0900

    :zap: sticked panel to right

--- MineSweeper_2022/Form1.Designer.cs
+++ MineSweeper_2022/Form1.Designer.cs
@@ -46,9 +46,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(100, 200);
+            this.pictureBox1.Location = new System.Drawing.Point(46, 100);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(555, 285);
+            this.pictureBox1.Size = new System.Drawing.Size(256, 142);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -56,9 +57,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("BIZ UDゴシック", 12F);
-            this.label1.Location = new System.Drawing.Point(100, 140);
+            this.label1.Location = new System.Drawing.Point(46, 70);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 32);
+            this.label1.Size = new System.Drawing.Size(55, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
@@ -66,18 +68,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("BIZ UDゴシック", 12F);
-            this.label2.Location = new System.Drawing.Point(450, 140);
+            this.label2.Location = new System.Drawing.Point(208, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 32);
+            this.label2.Size = new System.Drawing.Size(55, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
             // 
             // button1
             // 
             this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(150, 60);
+            this.button1.Location = new System.Drawing.Point(69, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.Size = new System.Drawing.Size(46, 50);
             this.button1.TabIndex = 3;
             this.button1.Text = "↑";
             this.button1.UseVisualStyleBackColor = true;
@@ -86,9 +90,10 @@
             // button2
             // 
             this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(30, 180);
+            this.button2.Location = new System.Drawing.Point(14, 90);
+            this.button2.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.Size = new System.Drawing.Size(46, 50);
             this.button2.TabIndex = 4;
             this.button2.Text = "←";
             this.button2.UseVisualStyleBackColor = true;
@@ -97,9 +102,10 @@
             // button3
             // 
             this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button3.Location = new System.Drawing.Point(270, 180);
+            this.button3.Location = new System.Drawing.Point(125, 90);
+            this.button3.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.Size = new System.Drawing.Size(46, 50);
             this.button3.TabIndex = 5;
             this.button3.Text = "→";
             this.button3.UseVisualStyleBackColor = true;
@@ -108,9 +114,10 @@
             // button4
             // 
             this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button4.Location = new System.Drawing.Point(150, 300);
+            this.button4.Location = new System.Drawing.Point(69, 150);
+            this.button4.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.Size = new System.Drawing.Size(46, 50);
             this.button4.TabIndex = 6;
             this.button4.Text = "↓";
             this.button4.UseVisualStyleBackColor = true;
@@ -118,9 +125,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(30, 450);
+            this.button5.Location = new System.Drawing.Point(14, 225);
+            this.button5.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(140, 50);
+            this.button5.Size = new System.Drawing.Size(65, 25);
             this.button5.TabIndex = 7;
             this.button5.Text = "オープン";
             this.button5.UseVisualStyleBackColor = true;
@@ -128,9 +136,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(230, 450);
+            this.button6.Location = new System.Drawing.Point(106, 225);
+            this.button6.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(140, 50);
+            this.button6.Size = new System.Drawing.Size(65, 25);
             this.button6.TabIndex = 8;
             this.button6.Text = "フラグ";
             this.button6.UseVisualStyleBackColor = true;
@@ -138,6 +147,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.button6);
             this.groupBox1.Controls.Add(this.button5);
             this.groupBox1.Controls.Add(this.button4);
@@ -145,9 +155,11 @@
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Font = new System.Drawing.Font("BIZ UDゴシック", 9F);
-            this.groupBox1.Location = new System.Drawing.Point(1900, 200);
+            this.groupBox1.Location = new System.Drawing.Point(877, 100);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(400, 550);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.groupBox1.Size = new System.Drawing.Size(185, 275);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "操作パネル";
@@ -157,18 +169,20 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("BIZ UDゴシック", 15F);
-            this.label3.Location = new System.Drawing.Point(2000, 800);
+            this.label3.Location = new System.Drawing.Point(923, 400);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(137, 40);
+            this.label3.Size = new System.Drawing.Size(69, 20);
             this.label3.TabIndex = 10;
             this.label3.Text = "label3";
             // 
             // button7
             // 
             this.button7.Font = new System.Drawing.Font("BIZ UDゴシック", 9F);
-            this.button7.Location = new System.Drawing.Point(2130, 80);
+            this.button7.Location = new System.Drawing.Point(983, 40);
+            this.button7.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(170, 50);
+            this.button7.Size = new System.Drawing.Size(78, 25);
             this.button7.TabIndex = 11;
             this.button7.Text = "リトライ";
             this.button7.UseVisualStyleBackColor = true;
@@ -176,15 +190,16 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2374, 1529);
+            this.ClientSize = new System.Drawing.Size(1132, 613);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.Name = "Form1";
             this.Text = "MineSweeper";
             this.Load += new System.EventHandler(this.Form1_Load);
