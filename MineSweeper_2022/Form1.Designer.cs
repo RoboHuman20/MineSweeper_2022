5dbe02d3124148e80f025f426679e75b46588d6a
fujii <robo_human@fuji.waseda.jp>
2022-12-30 00:32:02 +0900

    :lipstick: changed design

--- MineSweeper_2022/Form1.Designer.cs
+++ MineSweeper_2022/Form1.Designer.cs
@@ -31,17 +31,9 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button7 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -76,102 +68,12 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
             // 
-            // button1
-            // 
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(69, 30);
-            this.button1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(46, 50);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "↑";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(14, 90);
-            this.button2.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(46, 50);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "←";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button3.Location = new System.Drawing.Point(125, 90);
-            this.button3.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(46, 50);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "→";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button4.Location = new System.Drawing.Point(69, 150);
-            this.button4.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(46, 50);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "↓";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(14, 225);
-            this.button5.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(65, 25);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "オープン";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(106, 225);
-            this.button6.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(65, 25);
-            this.button6.TabIndex = 8;
-            this.button6.Text = "フラグ";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.button6);
-            this.groupBox1.Controls.Add(this.button5);
-            this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Font = new System.Drawing.Font("BIZ UDゴシック", 9F);
-            this.groupBox1.Location = new System.Drawing.Point(877, 100);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(1, 2, 1, 2);
-            this.groupBox1.Size = new System.Drawing.Size(185, 275);
-            this.groupBox1.TabIndex = 9;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "操作パネル";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("BIZ UDゴシック", 15F);
-            this.label3.Location = new System.Drawing.Point(923, 400);
+            this.label3.Location = new System.Drawing.Point(360, 50);
             this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 20);
@@ -182,7 +84,7 @@
             // 
             this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button7.Font = new System.Drawing.Font("BIZ UDゴシック", 9F);
-            this.button7.Location = new System.Drawing.Point(983, 40);
+            this.button7.Location = new System.Drawing.Point(450, 50);
             this.button7.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(78, 25);
@@ -195,10 +97,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1132, 613);
+            this.ClientSize = new System.Drawing.Size(580, 400);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
@@ -207,7 +108,6 @@
             this.Text = "MineSweeper";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
